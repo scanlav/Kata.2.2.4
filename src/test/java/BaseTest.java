ae1214dcfb857705d808b23803f3bcdbd8a13a67
Viktor Churikov <scanlave@gmail.com>
2023-07-13 18:49:29 +0300

    Задача решена

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class BaseTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BaseTest {
+
+    @Test
+    @DisplayName("testOne")
+    void testOne() {
+        System.out.println("testOne");
+    }
+
+    @Test
+    @DisplayName("testTwo")
+    void testTwo() {
+        System.out.println("testTwo");
+    }
 }
